2748d0ac8d2f6a330f899e52fd34c57538255448
Vineet Kumar <mr.vineet_kumar@live.com>
2018-03-21 17:59:32 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'my-app',
-  template:'<user></user>'              
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { 
-  
-  
- }
+export class AppComponent  { name = 'Angular'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
-import {UserComponent} from './components/user.component';
 
 @NgModule({
-  imports:      [ BrowserModule,FormsModule ],
-  declarations: [ AppComponent,UserComponent ],
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
