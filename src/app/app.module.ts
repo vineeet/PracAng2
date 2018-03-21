ab190fd046a92c54a5972c71f5bfa2ca9f2c3598
Vineet Kumar <v-vikum2@microsoft.com>
2018-03-21 16:48:07 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {UserComponent} from './components/user.component';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,FormsModule],
+  declarations: [ AppComponent, UserComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
